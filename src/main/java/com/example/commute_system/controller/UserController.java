09bf7eb7a2ce795dbfddb4f35ef62ff27192f5db
수찬 <soochan7@gmail.com>
2022-10-06 12:40:57 +0900

    JSP연결

--- src/main/java/com/example/commute_system/controller/UserController.java
+++ src/main/java/com/example/commute_system/controller/UserController.java
@@ -3,8 +3,6 @@ package com.example.commute_system.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @Controller
